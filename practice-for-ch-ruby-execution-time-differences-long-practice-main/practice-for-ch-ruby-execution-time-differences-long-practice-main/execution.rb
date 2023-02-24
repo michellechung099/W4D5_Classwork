4a26a972048398a9b76eb37a05591ccca4e72742
Taisia Karaseva <taisiat@gmail.com>
2023-02-24 12:23:28 -0800

    Add first answer for largest contiguous sum

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/practice-for-ch-ruby-execution-time-differences-long-practice-main/execution.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/practice-for-ch-ruby-execution-time-differences-long-practice-main/execution.rb
@@ -8,7 +8,7 @@ def my_min(list)
 end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min(list)
+# p my_min(list)
 
 #time complexity: linear
 def my_min(list)
@@ -22,4 +22,40 @@ def my_min(list)
 end
 
 list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min(list)
+# p my_min(list)
+
+
+#quadratic
+# def largest_contiguous_subsum(list)
+#   subs = []
+#   list.each.with_index do |ele, idx1|
+#     (idx1..list.length - 1).each do |idx2|
+#       subs << list[idx1..idx2]
+#     end
+#   end
+#   sums = subs.map { |sub| sub.inject(&:+) }
+#   sums.max
+# end
+
+
+def largest_contiguous_subsum(arr)
+  largest_sum = arr.first
+  current_sum = arr.first
+  #how to find index pairs without inner loops?
+  #slicing is linear right?
+  #current_sum + arr[i] - if this is bigger than largest sum then replace
+  (1..arr.length - 1).each do |idx|
+    current_sum += arr[idx]
+    largest_sum = current_sum if current_sum > largest_sum
+
+  end
+end
+
+list = [5, 3, -7]
+p largest_contiguous_subsum(list)
+
+list = [2, 3, -6, 7, -6, 7]
+p largest_contiguous_subsum(list)
+
+list = [-5, -1, -3]
+p largest_contiguous_subsum(list)
