8579aa07b30326bc8341011e7c41c96e92009012
mchung099 <mchung099@gmail.com>
2023-02-24 14:40:15 -0800

    execution finished

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/practice-for-ch-ruby-execution-time-differences-long-practice-main/execution.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/practice-for-ch-ruby-execution-time-differences-long-practice-main/execution.rb
@@ -7,7 +7,7 @@ def my_min(list)
   min
 end
 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min(list)
 
 #time complexity: linear
@@ -21,7 +21,7 @@ def my_min(list)
   min
 end
 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min(list)
 
 
@@ -38,24 +38,60 @@ list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # end
 
 
-def largest_contiguous_subsum(arr)
-  largest_sum = arr.first
-  current_sum = arr.first
-  #how to find index pairs without inner loops?
-  #slicing is linear right?
-  #current_sum + arr[i] - if this is bigger than largest sum then replace
-  (1..arr.length - 1).each do |idx|
-    current_sum += arr[idx]
-    largest_sum = current_sum if current_sum > largest_sum
+# def largest_contiguous_subsum(arr)
+#   largest_sum = arr.first
+#   current_sum = arr.first
+#   #how to find index pairs without inner loops?
+#   #slicing is linear right?
+#   #current_sum + arr[i] - if this is bigger than largest sum then replace
+#   (1..arr.length - 1).each do |idx|
+#     current_sum += arr[idx]
+#     largest_sum = current_sum if current_sum > largest_sum
 
+#   end
+# end
+
+def largest_contiguous_subsum(arr)
+  global_sum = arr.first
+  local_sum = arr.first
+  (1...arr.length).each do |i|
+    local_sum = 0 if local_sum < 0
+    local_sum += arr[i]
+    global_sum = local_sum if global_sum < local_sum
   end
+  global_sum
 end
 
+list = [2, 3, -6, 7, -6, 7]
+global = 2
+local = 2
+local 2 += 3 = 5
+global = 5
+
+i=2; -6
+global = 5
+local = 0 + -6 = -6
+global = 5
+
+i=3; 7
+local = 0 + 7 = 7
+global = 7
+
+i=4, -6
+local = 7 + -6 = 1
+global = 7
+
+i=5, 7
+local = 1 + 7
+global = 8 
+
+
+
 list = [5, 3, -7]
-p largest_contiguous_subsum(list)
+p largest_contiguous_subsum(list)  #8
 
 list = [2, 3, -6, 7, -6, 7]
-p largest_contiguous_subsum(list)
+p largest_contiguous_subsum(list)  #8
 
 list = [-5, -1, -3]
-p largest_contiguous_subsum(list)
+p largest_contiguous_subsum(list)  #-1
